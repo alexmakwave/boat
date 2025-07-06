633f468af4af449fce6331cc931c0cfce9456ffb
alexmakwave <alexmakwave@gmail.com>
2025-07-06 22:34:07 +0400

    deploy build

---
+++ telegram_wrapper.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Telegram Game</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://telegram.org/js/telegram-web-app.js"></script>
+  <style>
+    html, body {
+      margin: 0;
+      overflow: hidden;
+      height: 100%;
+    }
+    iframe {
+      width: 100%;
+      height: 100%;
+      border: none;
+      display: block;
+    }
+  </style>
+</head>
+<body>
+  <iframe src="build/index.html"></iframe>
+</body>
+</html>
